0f42ce33ef5b7b080c6700f0176425fa8d5f47e4
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-02-08 16:09:00 +0200

    style tweaks button

--- storage/framework/views/7dc5be37e84d8e28cbbaa3500f1180ed3dd24616.php
+++ storage/framework/views/7dc5be37e84d8e28cbbaa3500f1180ed3dd24616.php
@@ -16,14 +16,13 @@
 					<form method="GET" action="/threads/search">
 						<input type="text" style="box-sizing:inherit;margin-bottom	:12px;" placeholder="Search for something..." class="form-control"
 						 name="q" id="">
-						<button type="submit" class="btn shadow  btn-primary" style=" display:block;
+						<button type="submit" class="btn smth shadow btn-link" style=" text-decoration:none;display:block;
 						text-align:center ;
-						padding-top:2px;
-						
-						padding-bottom:2px;
-						margin:0 auto ;">
-							SEARCH
-
+					border-radius:5px;
+					margin:0 auto ;">
+						<i class="	glyphicon glyphicon-search
+						" style="-webkit-text-stroke: 1px white;height:1px;" aria-hidden="true"></i> SEARCH
+							
 						</button>
 					</form>
 				</div>
