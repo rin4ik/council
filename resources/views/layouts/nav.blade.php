3ee4bc5c595d5dab9925928b18f43a8b3826aa04
Mirahmad Mirzajonov <kvadro48@mail.ru>
2018-02-21 20:49:54 +0200

    navbar color

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-blue-darkest py-3">
+<nav class="bg-blue-darker py-3">
     <div class="container mx-auto flex justify-between items-center text-blue-lightest pl-6">
         <div>
             <h1 class="font-normal text-2xl">
