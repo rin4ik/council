b6a4127a3bd79df82a21307f2a86e7b509e66dbd
Mirakhmad Mirzajonov <kvadro48@mail.ru>
2018-02-21 18:47:24 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- storage/framework/views/7dc5be37e84d8e28cbbaa3500f1180ed3dd24616.php
+++ storage/framework/views/7dc5be37e84d8e28cbbaa3500f1180ed3dd24616.php
@@ -1,12 +1,12 @@
 <?php $__env->startSection('content'); ?>
-    <?php echo $__env->make('breadcrumbs', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <?php echo $__env->make('breadcrumbs', array_except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
     <div class="pt-6">
-        <?php echo $__env->make('threads._list', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+        <?php echo $__env->make('threads._list', array_except(get_defined_vars(), ['__data', '__path']))->render(); ?>
 
         <?php echo e($threads->render()); ?>
 
     </div>
 <?php $__env->stopSection(); ?>
 
-<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), ['__data', '__path']))->render(); ?>
